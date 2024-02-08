e8bf8847ef8126c8421189eddd5c27e63c5b89ec
Fritz Duchardt <fritz@duchardt.net>
2024-02-08 10:10:03 +0100

    Add clean master branch alias

--- config_rc.sh
+++ config_rc.sh
@@ -1,6 +1,6 @@
 current_dir=$(dirname "$0")
 # aliases
 alias {cleanbranches,cb}="$current_dir"/git/clean_branches.sh
-alias cbm="$current_dir"/git/clean_branches.sh master
+alias cbm="$current_dir/git/clean_branches.sh master"
 alias calc="$current_dir"/misc/calc.sh
 alias tax="$current_dir/misc/tax.sh calc"
--- git/clean_branches.sh
+++ git/clean_branches.sh
@@ -7,15 +7,12 @@ source "$SCRIPT_DIR/../lib/utils.sh"
 
 # Quick script to purge all the branches that have been merged to a target
 # branch (defaults to master)
-if [[ $# != 1 ]]; then
-  mergedInto="main"
-else
-  mergedInto="$1"
-fi
+mergeInto="${1:-main}"
 currentBranch=$(git symbolic-ref --short HEAD)
-log::info "currentBranch:$currentBranch"
+log::info "Merge Target: $mergeInto"
+log::info "Current Branch: $currentBranch"
 if [[ "${currentBranch}" != "${mergedInto}" ]]; then
-  echo "You have to be on the same git branch you're using as the merge target"
+  log::error "You have to be on the same git branch you're using as the merge target"
   exit 1
 fi
 lib::exec git branch --merged "${mergedInto}" | grep -v "^[*+]" | xargs -n 1 git branch -d
