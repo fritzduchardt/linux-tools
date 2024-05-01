5c55843612e645e7511e2dbc232e28c2d90cabb7
Fritz Duchardt <fritz@duchardt.net>
2024-05-01 08:46:04 +0200

    Improve clean branches

--- git/clean_branches.sh
+++ git/clean_branches.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 # shellcheck disable=SC1091
 source "$SCRIPT_DIR/../lib/log.sh"
 # shellcheck disable=SC1091
@@ -9,11 +9,16 @@ source "$SCRIPT_DIR/../lib/utils.sh"
 # Quick script to purge all the branches that have been merged to a target
 # branch (defaults to master)
 mergeInto="${1:-main}"
-currentBranch=$(git symbolic-ref --short HEAD)
-log::info "Merge Target: $mergeInto"
-log::info "Current Branch: $currentBranch"
-if [[ "$currentBranch" != "$mergeInto" ]]; then
-  log::error "You have to be on the same git branch you're using as the merge target"
+if ! lib::exec git checkout "$mergeInto"; then
+  log::error "Failed to checkout $mergeInto"
   exit 1
 fi
-lib::exec git branch --merged "$mergeInto" | grep -v "^[*+]" | xargs -n 1 git branch -d
+log::info "Merge Target: $mergeInto"
+branches="$(lib::exec git branch --merged "$mergeInto" | grep -v "^[*+]")"
+if [[ -z "$branches" ]]; then
+  log::info "No branches to delete"
+  exit 0
+fi
+while read -r branch; do
+  lib::exec git branch -d "$branch"
+done <<<"$branches"
