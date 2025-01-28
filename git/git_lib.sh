d60eedd4971e29ab0fb8d30f3809e69a7aa5b6c9
fritz duchardt <fritz@duchardt.net>
2025-01-28 09:59:16 +0100

    fix: find main branch

--- git/git_lib.sh
+++ git/git_lib.sh
@@ -2,17 +2,19 @@
 
 # Find project main branch
 find_main_branch() {
-	lib::exec git rev-parse --git-dir &>/dev/null || return
+	lib::exec git rev-parse --git-dir &> /dev/null || return
 	local ref
-	for ref in refs/remotes/origin/{main,master}; do
-		if lib::exec git show-ref -q --verify "$ref" 2>/dev/null; then
+	for ref in refs/{heads,remotes/{origin,upstream}}/{main,trunk,mainline,default,master}; do
+		if lib::exec git show-ref -q --verify "$ref" 2>/dev/null
+		then
 			echo "${ref##*/}"
 			return 0
 		fi
 	done
 	echo master
-	return 0
+	return 1
 }
+
 
 find_current_branch() {
 	lib::exec git rev-parse --abbrev-ref HEAD
