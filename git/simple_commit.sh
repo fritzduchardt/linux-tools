121a162303041be776d5f3d32c7c2c6aec951b90
Fritz Duchardt <fritz@duchardt.net>
2025-03-17 19:49:55 +0100

    feat: Enhanced git commit script with AI-powered message generation and CLI options
    - Added command line options (-m, -f, -n) for merge request, force push, and no-push modes
    - Integrated AI-powered commit message generation when no message is provided

--- git/simple_commit.sh
+++ git/simple_commit.sh
@@ -6,27 +6,57 @@ source "$SCRIPT_DIR/../lib/log.sh"
 source "$SCRIPT_DIR/../lib/utils.sh"
 source "$SCRIPT_DIR/git_lib.sh"
 
-msg="$*"
-if [[ -z "$msg" ]]; then
-  log::error "Please provide commit message"
-  exit 1
-fi
-prefix="$(echo -e "fix\nfeat\ndocs\nchore" | fzf)"
-mr="$(echo -e "no\nyes" | fzf --header "MR")"
-force="$(echo -e "no\nyes" | fzf --header "force")"
+while getopts "mfan" opt; do
+  case $opt in
+    m) mr="yes" ;;
+    f) force="yes" ;;
+    n) no_push="yes" ;;
+    *) exit 1 ;;
+  esac
+done
+shift $((OPTIND-1))
 
-cmd=(git push origin HEAD)
-if [[ "$mr" == "yes" ]]; then
-  main_branch="$(find_main_branch)"
-  if [[ "$(find_current_branch)" == "$main_branch" ]]; then
-    log::error "You are on $main_branch"
-    exit 2
+msg="$*"
+ai=""
+if [[ -z "$msg" ]]; then
+  log::info "Figuring out your commit message.."
+  msg_proposal=$(mktemp)
+  trap "rm $msg_proposal" EXIT
+  git diff --staged | "$SCRIPT_DIR/../fabric/fabric_stdin.sh" -p devops_gitcommit > "$msg_proposal"
+  vim "$msg_proposal"
+  msg=$(cat "$msg_proposal")
+  ai=true
+  if [[ -z "$msg" ]]; then
+    log::error "Please provide commit message"
+    exit 1
   fi
-  cmd+=(-o merge_request.create)
 fi
-if [[ "$force" == "yes" ]]; then
-  cmd+=(-f)
+
+if [[ -z "$ai" ]]; then
+  prefix_choices="fix\nfeat\ndocs\nchore"
+  prefix="$(echo -e "$prefix_choices" | fzf)"
+  msg="$prefix: $msg"
 fi
-lib::exec git add --all
-lib::exec git commit -m "$prefix: $msg"
-lib::exec "${cmd[@]}"
+
+if [[ -z "$mr" ]]; then
+  mr="$(echo -e "no\nyes" | fzf --header "MR")"
+fi
+
+lib::exec git commit -m "$msg"
+
+if [[ "$no_push" != "yes" ]]; then
+  cmd=(git push origin HEAD)
+  if [[ "$mr" == "yes" ]]; then
+    main_branch="$(find_main_branch)"
+    current_branch="$(find_current_branch)"
+    if [[ "$current_branch" == "$main_branch" ]]; then
+      log::error "You are on $main_branch"
+      exit 2
+    fi
+    cmd+=(-o merge_request.create)
+  fi
+  if [[ "$force" == "yes" ]]; then
+    cmd+=(-f)
+  fi
+  lib::exec "${cmd[@]}"
+fi
