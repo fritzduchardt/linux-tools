9f7c09d646aa0f940744d10816646fd85703fb4c
Fritz Duchardt <fritz@duchardt.net>
2025-03-21 11:34:11 +0100

    feat: Add pattern matching option (-p) to fabric_stdin.sh
    The change adds support for a new -p flag in the fabric_stdin.sh script that allows passing a pattern parameter to the underlying fabric command.

--- fabric/fabric_stdin.sh
+++ fabric/fabric_stdin.sh
@@ -8,13 +8,28 @@ FBRC_BIN="$SCRIPT_DIR/fabric.sh"
 
 # fbrcs
 function fbrc_stdin() {
-  local line prompt session
+  local line prompt session pattern
+
+  while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+      -p)
+        pattern="$2"
+        shift 2
+        ;;
+      *)
+        break
+        ;;
+    esac
+    prompt+="$1 "
+    shift
+  done
+
   while IFS= read -r line; do
     prompt="$prompt$line\n"
   done
   session="$(date +%Y%m%d%H%M%S)"
   trap "fabric --wipesession=$session" EXIT
-  lib::exec "$FBRC_BIN" "$@" -s "$session" "$prompt"
+  lib::exec "$FBRC_BIN" "$@" -p "$pattern" -s "$session" "$prompt"
 }
 
 fbrc_stdin "$@"
