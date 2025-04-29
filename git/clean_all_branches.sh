9c7265a4207c5e8a0b782a51310b57eecd294f8c
fritz duchardt <fritz@duchardt.net>
2025-04-29 16:23:15 +0200

    fix: clean all branches

--- git/clean_all_branches.sh
+++ git/clean_all_branches.sh
@@ -6,20 +6,22 @@ source "$SCRIPT_DIR/../lib/log.sh"
 source "$SCRIPT_DIR/../lib/utils.sh"
 source "$SCRIPT_DIR/git_lib.sh"
 
-delete_remote="$1"
+delete_remote="${1:-false}"
 main="$(find_main_branch)"
-if [[ -n "$main" ]]; then
+if [[ -z "$main" ]]; then
   log::error "Main branch not found"
   exit 1
 fi
 lib::exec git checkout "$main"
 while read -r branch; do
-  lib::prompt "Delete $branch"
-  if [[ "$delete_remote" == "true" ]]; then
-    if lib::exec git push origin --delete "$branch" &>/dev/null; then
-      log::info "Deleted on remote as well"
+  if lib::prompt "Delete $branch"; then
+    if [[ "$delete_remote" == "true" ]]; then
+      log::info "Delete branch: $branch"
+      lib::exec git branch -D "$branch"
+      if lib::exec git push origin --delete "$branch" &>/dev/null; then
+        log::info "Deleted on remote branch as well"
+      fi
     fi
   fi
-  lib::exec git branch -D "$branch"
 done < <(lib::exec git branch | grep -vw "$main")
 log::info "Finished"
--- git/git_lib.sh
+++ git/git_lib.sh
@@ -1,10 +1,14 @@
-# MINOR: Added error handling and simplified variable expansion
 find_main_branch() {
-	lib::exec git rev-parse --git-dir &> /dev/null || return 1
+	if ! lib::exec git rev-parse --git-dir >/dev/null; then
+		log::error "Not in git dir"
+		exit 2
+	fi
 	local ref
 	for ref in refs/{heads,remotes/{origin,upstream}}/{main,trunk,mainline,default,master}; do
-		if lib::exec git show-ref -q --verify "$ref" 2>/dev/null; then
-			echo "${ref##*/}"
+		if lib::exec git show-ref -q --verify "$ref" >/dev/null; then
+			local main_branch="${ref##*/}"
+			log::info "Found main branch: $main_branch"
+			echo "$main_branch"
 			return 0
 		fi
 	done
