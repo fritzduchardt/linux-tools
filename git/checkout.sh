2b817fe92e0a80024bd285ae172de4b79dbd06d9
Fritz Duchardt <fritz@duchardt.net>
2025-08-07 10:46:29 +0200

    fix: add fetch before git checkout

--- git/checkout.sh
+++ git/checkout.sh
@@ -20,6 +20,7 @@ function help() {
 
 function checkout_branches() {
   local branch="$1" co_branch branches
+  git fetch
   branches="$(lib::exec git branch -a --sort=-committerdate | tr -d ' ')"
   co_branch="$(echo "$branches" | fzf --query "$branch" --preview 'git log -n 10 --color=always --oneline --abbrev-commit {}' | sed "s/remotes\/origin\///g")"
   if [[ -n "$co_branch" ]]; then
@@ -33,4 +34,3 @@ if [[ "$1" == "-h" || "$1" == "--help" ]]; then
 fi
 
 checkout_branches "$1"
-
